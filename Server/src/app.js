0b55bff6b7c8ebbe972a03435bea8b76103ab8b2
hackrosseti <hackrosseti@gmail.com>
2020-11-27 19:18:10 +0300

    push 27.11.2020  update cors

--- Server/src/app.js
+++ Server/src/app.js
@@ -19,6 +19,7 @@ const connectToDataBase = () => {
 const ALLOWED_ORIGINS = [
   'http://168.63.58.52:80',
   'http://localhost:63342',
+  'http://localhost:80',
   'http://ksutechrosset.northeurope.cloudapp.azure.com'
 ];
 
